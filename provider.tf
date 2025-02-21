7fa0068ccf1b1add639d71c224d1ca7da4a8e275
Sujetha Pai Krishna Pai <122850668+suthapai@users.noreply.github.com>
2025-02-21 12:45:08 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,12 +8,12 @@ terraform {
     }
 
   }
-  backend "azurerm" {
+#  backend "azurerm" {
    # resource_group_name  = "chechrg"                # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     # storage_account_name = "chechsa"                # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     # container_name       = "blob"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     # key                  = "" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+ # }
 }
 provider "azurerm" {
   features {
